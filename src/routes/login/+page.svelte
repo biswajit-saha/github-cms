4db8002f9749c63603527a35e6c11ca73e6611a5
Biswajit Saha <gbjsolution@gmail.com>
2022-09-04 02:23:13 +0530

    Designed new login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,11 @@
+<div class="flex min-h-screen items-center justify-center bg-gray-50 py-16 px-8">
+	<div class="bg-white p-8 rounded-lg shadow w-full max-w-sm">
+		<div>
+			<img src="/logo-icon.svg" alt="Logo" class="mx-auto w-10 mb-8" />
+		</div>
+		<div class="text-center">
+			<h1 class="text-lg mb-8">Welcome to the Github admin</h1>
+			<a href="/api/auth/login" class="btn btn-full">Login with github</a>
+		</div>
+	</div>
+</div>
