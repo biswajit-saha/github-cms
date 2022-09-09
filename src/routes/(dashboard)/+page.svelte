df73dc0d3a1ecd51bf3d5029b98e027ac84739ff
Biswajit Saha <gbjsolution@gmail.com>
2022-09-09 18:05:13 +0530

    Small code clinup

--- src/routes/(dashboard)/+page.svelte
+++ src/routes/(dashboard)/+page.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
 	import Collection from '$lib/components/Collection.svelte';
-	// import ContentArea from '$lib/components/ContentArea.svelte';
-	// import DashboardBase from '$lib/components/DashboardBase.svelte';
-	// import Sidebar from '$lib/components/Sidebar.svelte';
-
-	export let data: { user: Object };
-	console.log(data);
 </script>
 
 <Collection />
