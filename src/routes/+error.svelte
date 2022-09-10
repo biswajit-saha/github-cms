f55f3b8b8d5bc2dffa38bd35992a1481998694fc
Biswajit Saha <gbjsolution@gmail.com>
2022-09-11 00:12:45 +0530

    Added back to home button

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,6 +5,7 @@
 <div class="flex min-h-screen items-center justify-center py-16 px-8">
 	<div class="text-center">
 		<h1 class="text-8xl text-slate-500 mb-2">{$page.status}</h1>
-		<div class="text-lg">{$page.error.message}</div>
+		<div class="text-lg mb-4">{$page.error.message}</div>
+		<a href="/" class="btn btn-sm"><span>Back to home</span></a>
 	</div>
 </div>
