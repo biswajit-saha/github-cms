9528d07ce95457ecb81220be348c05531a2d56e0
Biswajit Saha <gbjsolution@gmail.com>
2022-09-11 00:09:10 +0530

    Created a custom error page

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -5,7 +5,7 @@
 
 <aside class="w-[300px] bg-white absolute h-full shadow-sm">
 	<div class="p-5">
-		<img class="logo" src="/logo-icon.svg" alt="Logo" />
+		<a href="/"><img class="logo" src="/logo-icon.svg" alt="Logo" /></a>
 	</div>
 	<Menu />
 	<UserInfo />
---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex min-h-screen items-center justify-center py-16 px-8">
+	<div class="text-center">
+		<h1 class="text-8xl text-slate-500 mb-2">{$page.status}</h1>
+		<div class="text-lg">{$page.error.message}</div>
+	</div>
+</div>
