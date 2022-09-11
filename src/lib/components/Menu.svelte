34b69a7d9d9fbd6c2b68282bb839130f815d1259
Biswajit Saha <gbjsolution@gmail.com>
2022-09-12 03:36:48 +0530

    Improved sidebar style

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -10,12 +10,12 @@
 	});
 </script>
 
-<div class="mt-4">
+<div class="mt-4 mb-[70px] overflow-y-auto">
 	<ul class="">
 		{#each menus as menu (menu.id)}
 			<li class="menu-item">
-				<a href={menu.url} class="current-item"
-					><svg
+				<a href={menu.url} class="current-item">
+					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
 						viewBox="0 0 24 24"
@@ -29,8 +29,8 @@
 							d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
 						/>
 					</svg>
-					<span>{menu.label}</span></a
-				>
+					<span>{menu.label}</span>
+				</a>
 			</li>
 		{/each}
 	</ul>
