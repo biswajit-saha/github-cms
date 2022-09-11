f74e3ff05fe75a6896cdfcdf0533522213d2db75
Biswajit Saha <gbjsolution@gmail.com>
2022-09-12 03:43:14 +0530

    Merge branch 'main' into edit-page

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -3,7 +3,7 @@
 	import UserInfo from './UserInfo.svelte';
 </script>
 
-<aside class="w-[300px] bg-white absolute h-full shadow-sm">
+<aside class="w-[300px] bg-white fixed h-full shadow-sm flex flex-col">
 	<div class="p-5">
 		<a href="/"><img class="logo" src="/logo-icon.svg" alt="Logo" /></a>
 	</div>
